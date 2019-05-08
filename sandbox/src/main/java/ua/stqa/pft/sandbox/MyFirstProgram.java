76021275c0a601531e48b4b9cf3ef9bcc53edb2a
Artiukhov86 <50213469+Artiukhov86@users.noreply.github.com>
2019-05-08 17:19:24 +0300

    Тестовые изменения

---
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package ua.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Forza Roma");
+        System.out.println("Forza Roma Ale");
+
+
+    }
+}
