21cd3adaf7d83ff441f868c717f8c64dbd91e86d
Artiukhov86 <50213469+Artiukhov86@users.noreply.github.com>
2019-05-09 08:47:59 +0300

    удалил лишнее

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,11 +5,7 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        System.out.println("Forza Roma");
-        System.out.println("Forza Roma Ale");
-        System.out.println("Forza Roma Ale");
-
-        System.out.println(2 + 2);
+        
 
 
 
